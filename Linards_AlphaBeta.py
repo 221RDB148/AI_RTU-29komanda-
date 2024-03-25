beca10d6153d632a0eda53c2c3c814dfda788b27
Linards657 <naglis.linards@gmail.com>
2024-03-25 23:15:26 +0200

    Iespejami fixi

--- Linards_AlphaBeta.py
+++ Linards_AlphaBeta.py
@@ -5,9 +5,9 @@ from functools import lru_cache
 
 
 @lru_cache(10000)
-def alphabeta(game_tree, level, node_id, depth, is_maximizing, alpha=-math.inf, beta=math.inf):
+def alphabeta(game_tree, level, node_id, depth, is_maximizing, start_level, alpha=-math.inf, beta=math.inf):
     if depth == 0 or not game_tree[level][node_id].children:
-        if level % 2 != 0:
+        if start_level % 2 != 0:
             return (game_tree[level][node_id].p2_points + game_tree[level][node_id].p2_rocks) - (
                     game_tree[level][node_id].p1_points + game_tree[level][node_id].p1_rocks), None
         else:
@@ -16,7 +16,7 @@ def alphabeta(game_tree, level, node_id, depth, is_maximizing, alpha=-math.inf, 
     if is_maximizing:
         max_eval = -math.inf
         for node_id2 in game_tree[level][node_id].children:
-            score_eval = alphabeta(game_tree, level + 1, node_id2, depth - 1, False)[0]
+            score_eval = alphabeta(game_tree, level + 1, node_id2, depth - 1, False, start_level)[0]
             if score_eval > max_eval:
                 ai_move = game_tree[level + 1][node_id2]
                 max_eval = score_eval
@@ -27,7 +27,7 @@ def alphabeta(game_tree, level, node_id, depth, is_maximizing, alpha=-math.inf, 
     else:
         min_eval = math.inf
         for node_id2 in game_tree[level][node_id].children:
-            score_eval = alphabeta(game_tree, level + 1, node_id2, depth - 1, True)[0]
+            score_eval = alphabeta(game_tree, level + 1, node_id2, depth - 1, True, start_level)[0]
             if score_eval < min_eval:
                 ai_move = game_tree[level + 1][node_id2]
                 min_eval = score_eval
@@ -38,24 +38,20 @@ def alphabeta(game_tree, level, node_id, depth, is_maximizing, alpha=-math.inf, 
 
 
 def main():
-    akmeni = 70
+    akmeni = 2
     game_tree_start = generate_game_tree(akmeni)
-    max_depth = 50
+    max_depth = 70
     levels = 0
-    Used_moves = []
     start = time.time()
-    print(game_tree_start[0][0])
-    rock_buffer = game_tree_start[0][0].rocks
-    move = alphabeta(game_tree_start, 0, 0, max_depth, True)[1]
     move = game_tree_start[0][0]
-    print("P1 takes", rock_buffer - move.rocks, " and moves =")
     print(move)
+    Used_moves = []
 
     while 1:
         if not move.children:
             break
         rock_buffer = move.rocks
-        move = alphabeta(game_tree_start, levels, move.node_id, max_depth, True)[1]
+        move = alphabeta(game_tree_start, levels, move.node_id, max_depth, True, levels)[1]
         if levels % 2 == 0:
             print("P1 takes", rock_buffer - move.rocks, " and moves =")
         else:
@@ -63,6 +59,10 @@ def main():
         print(move)
         Used_moves.append(rock_buffer - move.rocks)
         levels += 1
+
+    print(move.p1_points + move.p1_rocks, move.p2_points + move.p2_rocks)
+    print(Used_moves)
+    print(len(Used_moves))
     end = time.time()
     print("___________________")
     print(end - start)
--- Linards_minmax.py
+++ Linards_minmax.py
@@ -5,9 +5,9 @@ from functools import lru_cache
 
 
 @lru_cache(10000)
-def minimax(game_tree, level, node_id, depth, is_maximizing):
+def minimax(game_tree, level, node_id, depth, is_maximizing, start_level):
     if depth == 0 or not game_tree[level][node_id].children:
-        if level % 2 != 0:
+        if start_level % 2 != 0:
             return (game_tree[level][node_id].p2_points + game_tree[level][node_id].p2_rocks) - (
                     game_tree[level][node_id].p1_points + game_tree[level][node_id].p1_rocks), None
         else:
@@ -16,7 +16,7 @@ def minimax(game_tree, level, node_id, depth, is_maximizing):
     if is_maximizing:
         max_eval = -inf
         for node_id2 in game_tree[level][node_id].children:
-            score_eval = minimax(game_tree, level + 1, node_id2, depth - 1, False)[0]
+            score_eval = minimax(game_tree, level + 1, node_id2, depth - 1, False, start_level)[0]
             if score_eval > max_eval:
                 ai_move = game_tree[level + 1][node_id2]
                 max_eval = score_eval
@@ -24,7 +24,7 @@ def minimax(game_tree, level, node_id, depth, is_maximizing):
     else:
         min_eval = inf
         for node_id2 in game_tree[level][node_id].children:
-            score_eval = minimax(game_tree, level + 1, node_id2, depth - 1, True)[0]
+            score_eval = minimax(game_tree, level + 1, node_id2, depth - 1, True, start_level)[0]
             if score_eval < min_eval:
                 ai_move = game_tree[level + 1][node_id2]
                 min_eval = score_eval
@@ -32,15 +32,12 @@ def minimax(game_tree, level, node_id, depth, is_maximizing):
 
 
 def main():
-    akmeni = 70
+    akmeni = 2
     game_tree_start = generate_game_tree(akmeni)
-    max_depth = 50
+    max_depth = 70
     levels = 0
     start = time.time()
-    print(game_tree_start[0][0])
-    rock_buffer = game_tree_start[0][0].rocks
     move = game_tree_start[0][0]
-    print("P1 takes", rock_buffer - move.rocks, " and moves =")
     print(move)
     Used_moves = []
 
@@ -48,16 +45,17 @@ def main():
         if not move.children:
             break
         rock_buffer = move.rocks
-        move = minimax(game_tree_start, levels, move.node_id, max_depth, True)[1]
+        move = minimax(game_tree_start, levels, move.node_id, max_depth, True, levels)[1]
         if levels % 2 == 0:
             print("P1 takes", rock_buffer - move.rocks, " and moves =")
         else:
             print("P2 takes", rock_buffer - move.rocks, "and moves =")
         print(move)
-        Used_moves.append(move)
+        Used_moves.append(rock_buffer - move.rocks)
         levels += 1
 
     print(move.p1_points + move.p1_rocks, move.p2_points + move.p2_rocks)
+    print(Used_moves)
     print(len(Used_moves))
     end = time.time()
     print("___________________")
